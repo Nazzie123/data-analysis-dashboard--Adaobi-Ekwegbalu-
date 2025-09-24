cfcc2cb3fdb80adf8df11abc91ae0c0eb85e7fd8
Nazzie123 <nazzie41@gmail.com>
2025-09-23 19:26:07 -0500

    BVCC Week 1 Assignment

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+
+
+function Footer(){
+    return (
+        <footer>
+            <div className="px-20 py-6 fixed bottom-0 w-full m bg-gradient-to-br from-gray-50 to-gray-100">
+                &copy;<span className="text-gray-600">Adaobi Ekwegbalu</span> | Built with React
+            </div>
+        </footer>
+    )
+}
+
+
+export default Footer;
